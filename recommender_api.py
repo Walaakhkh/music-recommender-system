d76681589b466a6846705b6c54490496f5969f4c
WALAAKHKH <Walaakhalil98@gmail.com>
2024-09-23 13:04:17 -0700

    flask app

--- recommender_api.py
+++ recommender_api.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-# recommender_api.py
+#A simple Flask API for recommending artists using implicit recommender system
+
+# Import necessary libraries and modules
 from flask import Flask, request, jsonify
 from recommenderbackend.recommender import ImplicitRecommender
 from recommenderbackend.data import load_user_artists, ArtistRetriever      
 from pathlib import Path
 import implicit
 
+# Create an instance of the Flask app
 app = Flask(__name__, static_folder='frontend', static_url_path='')
 
 # Load data
@@ -18,10 +21,12 @@ model = implicit.als.AlternatingLeastSquares(factors=50, iterations=10, regulari
 recommender = ImplicitRecommender(artist_retriever, model)
 recommender.fit(user_artists)
 
+# Define a route for the home page, serving the static 'index.html' file
 @app.route('/')
 def home():
     return app.send_static_file('index.html')
 
+# Define a route for generating artist recommendations based on user ID
 @app.route('/recommend', methods=['GET'])
 def recommend():
     user_id = int(request.args.get('user_id', 1))  # Get user ID from query parameter
@@ -34,5 +39,6 @@ def recommend():
         "recommendations": [{"artist": artist, "score": score} for artist, score in zip(artists, scores)]
     })
 
+# Run the Flask app on port 5002
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
